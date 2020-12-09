720b80d60d8d9d26070e76c6521b08c3360853d7
Jens Utbult <jens.utbult@yubico.com>
2020-12-09 10:13:37 +0100

    Reading, writing and deleting OATH credentials from Yubikey implemented.

--- SimpleOATHDemo/CredentialsProvider.swift
+++ SimpleOATHDemo/CredentialsProvider.swift
@@ -1,19 +1,109 @@
-import Foundation
+import SwiftUI
 
-class CredentialsProvider: ObservableObject {
+class CredentialsProvider: NSObject, ObservableObject, YKFManagerDelegate {
     
-    @Published var credentials = Credential.previewCredentials()
+    @Published var credentials = [Credential]()
 
-    func refresh() {
-        credentials = Credential.previewCredentials()
+    override init() {
+        super.init()
+        YubiKitManager.shared.delegate = self
+        YubiKitManager.shared.startAccessoryConnection()
     }
     
-    func delete(credential: Credential) {
-        credentials.removeAll { $0.id == credential.id }
+    var nfcConnection: YKFNFCConnection?
+
+    func didConnectNFC(_ connection: YKFNFCConnection) {
+        nfcConnection = connection
+        if let callback = connectionCallback {
+            callback(connection)
+        }
+    }
+    
+    func didDisconnectNFC(_ connection: YKFNFCConnection, error: Error?) {
+        nfcConnection = nil
+        session = nil
+    }
+    
+    var accessoryConnection: YKFAccessoryConnection?
+
+    func didConnectAccessory(_ connection: YKFAccessoryConnection) {
+        accessoryConnection = connection
+        refresh()
+    }
+    
+    func didDisconnectAccessory(_ connection: YKFAccessoryConnection, error: Error?) {
+        accessoryConnection = nil
+        session = nil
+        credentials.removeAll()
+    }
+    
+    var connectionCallback: ((_ connection: YKFConnectionProtocol) -> Void)?
+    
+    func connection(completion: @escaping (_ connection: YKFConnectionProtocol) -> Void) {
+        if let connection = accessoryConnection {
+            completion(connection)
+        } else {
+            connectionCallback = completion
+            YubiKitManager.shared.startNFCConnection()
+        }
+    }
+    
+    var session: YKFKeyOATHSession?
+
+    func session(completion: @escaping (_ session: YKFKeyOATHSession?, _ error: Error?) -> Void) {
+        if let session = session {
+            completion(session, nil)
+            return
+        }
+        connection { connection in
+            connection.oathSession { session, error in
+                self.session = session
+                completion(session, error)
+            }
+        }
+    }
+    
+    func refresh() {
+        session { session, error in
+            guard let session = session else { print("Error: \(error!)"); return }
+            session.calculateAll { calculatedCredentials, error in
+                YubiKitManager.shared.stopNFCConnection()
+                guard let calculatedCredentials = calculatedCredentials else { print("Error: \(error!)"); return }
+                DispatchQueue.main.async {
+                    self.credentials = calculatedCredentials.map { Credential(issuer: $0.credential.issuer, accountName: $0.credential.accountName, otp: $0.code?.otp) }
+                }
+            }
+        }
     }
     
     func add(credential: Credential) {
-        credentials.append(credential)
+        session { session, error in
+            guard let session = session else { print("Error: \(error!)"); return }
+            let secret = NSData.ykf_data(withBase32String: "asecretsecret")!
+            let credentialTemplate = YKFOATHCredentialTemplate(totpWith: .SHA512, secret: secret, issuer: credential.issuer, accountName: credential.accountName)
+            session.put(credentialTemplate, requiresTouch: false) { error in
+                guard error == nil else { print("Error: \(error!)"); return }
+                self.refresh()
+            }
+        }
+    }
+    
+    func delete(credential: Credential) {
+        session { session, error in
+            guard let session = session else { print("Error: \(error!)"); return }
+            let oathCredential = YKFOATHCredential()
+            oathCredential.accountName = credential.accountName
+            oathCredential.issuer = credential.issuer
+            session.delete(oathCredential) { error in
+                YubiKitManager.shared.stopNFCConnection()
+                guard error == nil else { print("Error: \(error!)"); return }
+                if let index = self.credentials.firstIndex(of: credential) {
+                    DispatchQueue.main.async {
+                        self.credentials.remove(at: index)
+                    }
+                }
+            }
+        }
     }
 }
 
